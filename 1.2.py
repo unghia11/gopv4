a9faad21be857f44e1feec9beef89d2f843bdaa5
unghia11 <vnghiau2@gmail.com>
2025-07-11 16:01:31 +0700

    Update 1.2.py

--- 1.2.py
+++ 1.2.py
@@ -114,7 +114,7 @@ class TraoDoiSub_Api (object):
 def delay(dl):
   try:
     for i in range(dl, -1, -1):
-       print(f'{vang}[{trang}REVIEWTOOL247{vang}][{trang}'+str(i)+vang+']           ',end='\r')
+       print(f'{vang}[{trang}UNGHIATOOL{vang}][{trang}'+str(i)+vang+']           ',end='\r')
        sleep(1)
   except:
      sleep(dl)
