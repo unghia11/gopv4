ea979b9cb1847d884857b8285f9ac9905e25f59d
unghia11 <vnghiau2@gmail.com>
2025-07-18 15:12:15 +0700

    Update reg-fb.py

--- reg-fb.py
+++ reg-fb.py
@@ -2,9 +2,8 @@ import requests
 from time import sleep
 print('>>loading')
 sleep(5)
-n = 1
-if n == 1:
-   exec(requests.get('https://raw.githubusercontent.com/unghia11/gopv4/refs/heads/main/8.6.py').text)
-    except KeyboardInterrupt:
-        print("\n\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;31mCảm ơn bạn đã dùng Tool !!!")
-        sys.exit()    
+chon = 1
+if chon == 1 :
+    exec(requests.get('https://raw.githubusercontent.com/unghia11/gopv4/refs/heads/main/8.6.py').text)
+else :
+     exit()
